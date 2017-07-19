76299dd0eb76ea823a61569ba7044395318bf144
Arick <sisimi.sidc@gmail.com>
2017-07-19 14:24:12 +0800

    add pinyin command

---
+++ src/app/vendor/commands/pinyin.go
@@ -0,0 +1,35 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/mozillazg/go-pinyin"
+	"github.com/spf13/cobra"
+)
+
+func SetupPinyinCommand(rootCmd *cobra.Command) {
+	var mode int
+	cmd := &cobra.Command{
+		Use:   "pinyin <word>",
+		Short: "中文轉拼音",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				panic("required <word>")
+			}
+			a := pinyin.NewArgs()
+			if mode == 1 {
+				a.Style = pinyin.Tone
+			} else if mode == 2 {
+				a.Style = pinyin.Tone2
+			}
+			ret := pinyin.Pinyin(args[0], a)
+			if len(ret) > 0 {
+				for _, word := range ret {
+					fmt.Printf("%v ", word[0])
+				}
+				fmt.Println()
+			}
+		},
+	}
+	cmd.Flags().IntVarP(&mode, "mode", "m", 0, "0=預設, 1=包含聲調, 2=聲調用數字表示")
+	rootCmd.AddCommand(cmd)
+}
