7c6f072824cb5a9ca7f0a22495bd46003e99b279
Arick <sisimi.sidc@gmail.com>
2017-07-20 00:14:29 +0800

    支援從 stdin 讀取播放列表

--- src/app/vendor/commands/mp3/play.go
+++ src/app/vendor/commands/mp3/play.go
@@ -5,50 +5,79 @@ import (
 	"github.com/hajimehoshi/oto"
 	"github.com/spf13/cobra"
 	"io"
+    "bufio"
+    "strings"
 	"os"
+    "fmt"
 )
 
 // https://github.com/hajimehoshi/go-mp3/blob/master/example/main.go
+
+func play(fn string) {
+    f, err := os.Open(fn)
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+
+    d, err := mp3.NewDecoder(f)
+    if err != nil {
+        panic(err)
+    }
+    defer d.Close()
+
+    p, err := oto.NewPlayer(d.SampleRate(), 2, 2, 8192)
+    if err != nil {
+        panic(err)
+    }
+    defer p.Close()
+
+
+    if _, err := io.Copy(p, d); err != nil {
+        panic(err)
+    }
+}
 func SetupPlayCommand(rootCmd *cobra.Command) {
 	var repeat int
 	cmd := &cobra.Command{
 		Use:   "play <filename>",
 		Short: "播放mp3",
 		Run: func(cmd *cobra.Command, args []string) {
+            var playlist []string
 			if len(args) < 1 {
-				panic("required <filename>")
-			}
-
-			f, err := os.Open(args[0])
-			if err != nil {
-				panic(err)
-			}
-			defer f.Close()
-
-			d, err := mp3.NewDecoder(f)
-			if err != nil {
-				panic(err)
-			}
-			defer d.Close()
-
-			p, err := oto.NewPlayer(d.SampleRate(), 2, 2, 8192)
-			if err != nil {
-				panic(err)
-			}
-			defer p.Close()
-
-			c := 0
-			for {
-				if _, err := io.Copy(p, d); err != nil {
-					panic(err)
-				}
-				c += 1
-				if repeat > 0 && c >= repeat {
-					break
-				}
-
-				d.Seek(0, io.SeekStart)
-			}
+				reader := bufio.NewReader( os.Stdin )
+                
+                for {
+                    line, err := reader.ReadString('\n')
+                    if len(line) > 0 {
+                        // fmt.Println(line)
+                        playlist = append(playlist, strings.TrimSpace(line))
+                    }
+                    
+                    if err == io.EOF {
+                        break
+                    }
+                    
+                    if err != nil {
+                        panic(err)
+                    }
+                }
+			}else{
+                playlist = args
+            }
+            fmt.Printf("Music: %d\n", len(playlist) )
+            c := 1
+            for {
+                for _, fn := range playlist {
+                    fmt.Printf("Playing [%s]\n", fn)
+                    play(fn)
+                }
+                
+                if repeat > 0 && c >= repeat {
+                    break
+                }
+                c += 1
+            }
 		},
 	}
 
