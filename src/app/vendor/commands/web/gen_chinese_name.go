c951895a49b5c305c7379ab032e99380d61b96d4
cwchiu <sisimi.pchome@gmail.com>
2017-10-19 17:49:35 +0800

    windows 常用指令腳本

--- src/app/vendor/commands/web/gen_chinese_name.go
+++ src/app/vendor/commands/web/gen_chinese_name.go
@@ -23,7 +23,7 @@ func SetupGenChineseNameCommand(rootCmd *cobra.Command) {
 			// form.Add("break", "4")
 			// form.Add("name_count", "100")
 			resp, _, err := request.Post("http://www.richyli.com/name/index.asp").Set("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)").
-				Type("urlencoded").Send(form.Encode()).SetDebug(true).End()
+				Type("urlencoded").Send(form.Encode()).SetDebug(false).End()
 			if err != nil {
 				panic(err)
 			}
