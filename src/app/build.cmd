e15260443a2e0ab353179973c020c6d80a19d6df
cwchiu <sisimi.pchome@gmail.com>
2017-10-19 11:23:44 +0800

    build script update

--- src/app/build.cmd
+++ src/app/build.cmd
@@ -1,4 +1,25 @@
 @echo off
 
 SET GOPATH="%~dp0..\.."
-go build -o tool-debug.exe
+
+IF "%GOOS%"=="" SET GOOS=windows
+IF "%GOARCH%"=="" SET GOARCH=amd64
+SET VER=-debug
+SET OPT=
+IF "%BUILD%"=="RELEASE" SET VER=-release
+IF "%BUILD%"=="RELEASE" SET OPT=-ldflags "-w -s"
+IF "%BUILD%"=="RELEASE" SET UPX_EXE=upx.exe
+
+SET EXT=.exe
+IF not "%GOOS%"=="windows" SET EXT=
+
+SET ARM=
+IF not "%GOARM%"=="" SET ARM=-arm%GOARM%
+
+SET OUT_EXE=tool-%GOOS%-%GOARCH%%VER%%ARM%%EXT%
+
+echo Build "%OUT_EXE%" ...
+
+go build %OPT% -o %OUT_EXE%
+
+if exist %UPX_EXE% %UPX_EXE% -9 %OUT_EXE%
